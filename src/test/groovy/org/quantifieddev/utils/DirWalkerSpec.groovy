2eef35da206a7e0a26c6e228dda2c0a81012ba03
Chinmay Naik <cnaik@equalexperts.com>
2014-05-22 15:42:16 +0530

    added test for DirWalker

---
+++ src/test/groovy/org/quantifieddev/utils/DirWalkerSpec.groovy
@@ -0,0 +1,20 @@
+package org.quantifieddev.utils
+
+import spock.lang.Specification
+
+import java.util.regex.Pattern
+
+class DirWalkerSpec extends Specification {
+
+    def "it returns files matching specified regex in a given directory"() {
+        given:
+        def projectRoot = new File("src/test/resources").getAbsolutePath() + "/SampleProject"
+        DirWalker dirWalker = new DirWalker(new URI("file://$projectRoot"), Pattern.compile("(.*\\.java)|(.*\\.groovy)"));
+
+        when:
+        def files = dirWalker.walk()
+
+        then:
+        files == ["Sample.groovy", "Hello.java"]
+    }
+}
