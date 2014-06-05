cbda1cad528343da1212bf6ad895406a4c0b4bfb
Chinmay Naik <cnaik@equalexperts.com>
2014-06-05 15:43:43 +0530

    - encoding the streamId and readToken from the plugin now.

--- src/main/groovy/org/quantifieddev/idea/ToolWindowFactory.groovy
+++ src/main/groovy/org/quantifieddev/idea/ToolWindowFactory.groovy
@@ -14,19 +14,10 @@ import org.quantifieddev.utils.DateFormat
 import org.quantifieddev.utils.EventLogger
 
 import javax.imageio.ImageIO
-import javax.swing.ImageIcon
-import javax.swing.JEditorPane
-import javax.swing.JScrollPane
+import javax.swing.*
 import javax.swing.event.HyperlinkEvent
 import javax.swing.event.HyperlinkListener
-import javax.swing.JToolBar
 import java.awt.*
-import javax.swing.JButton
-import javax.swing.JPanel
-import javax.swing.JToggleButton
-import java.awt.Component
-import java.awt.Dimension
-import java.awt.GridBagLayout
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.awt.event.ItemEvent
@@ -53,7 +44,7 @@ class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory {
         contentPanel = new JPanel(new GridBagLayout())
 
         wtfButton = new JButton()
-        Image wtfImage= ImageIO.read(getClass().getResource('/wtf_icon75x45.jpg'))
+        Image wtfImage = ImageIO.read(getClass().getResource('/wtf_icon75x45.jpg'))
         wtfButton.setIcon(new ImageIcon(wtfImage))
         wtfButton.setToolTipText("Log WTF!")
         contentPanel.add(wtfButton)
@@ -69,21 +60,21 @@ class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory {
         toolBar.setFloatable(false)
 
         settingsButton = new JButton()
-        Image settingsImage= ImageIO.read(getClass().getResource('/settings_24x24.png'))
+        Image settingsImage = ImageIO.read(getClass().getResource('/settings_24x24.png'))
         settingsButton.setMargin(new Insets(0, 0, 0, 0))
         settingsButton.setIcon(new ImageIcon(settingsImage))
         settingsButton.setToolTipText("Edit/View QD Settings")
         toolBar.add(settingsButton)
 
         qdButton = new JButton()
-        Image qdImage= ImageIO.read(getClass().getResource('/QDLogo_24x24.png'))
+        Image qdImage = ImageIO.read(getClass().getResource('/QDLogo_24x24.png'))
         qdButton.setMargin(new Insets(0, 0, 0, 0))
         qdButton.setIcon(new ImageIcon(qdImage))
         qdButton.setToolTipText("View my QD dashboard in browser")
         toolBar.add(qdButton)
 
         helpToggleButton = new JToggleButton()
-        Image helpImage= ImageIO.read(getClass().getResource('/help_24x24.png'))
+        Image helpImage = ImageIO.read(getClass().getResource('/help_24x24.png'))
         helpToggleButton.setMargin(new Insets(0, 0, 0, 0))
         helpToggleButton.setIcon(new ImageIcon(helpImage))
         helpToggleButton.setToolTipText("About WTF")
@@ -92,14 +83,16 @@ class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory {
     }
 
     private JScrollPane setupHelpPane() {
+        def encodedStreamId = URLEncoder.encode(settings.streamId, "UTF-8")
+        def encodedReadToken = URLEncoder.encode(settings.readToken, "UTF-8")
         def message = """
                        | This QuantifiedDev plug-in lets you log and compare your software development activity for personal insights. We are currently in beta and are releasing new features all the time.
                        | <p>
                        | Current features:
                        | <ol>
-                       | <li>WTFs - Wtf is a way of <a href='http://www.quantifieddev.org/images/wtfs_per_minute.gif?plugin=intellij'>measuring code quality</a>. Hit the wtf button every time you see something you don't like or find confusing, then review you wtfs over time on <a href='http://www.quantifieddev.org/app/dashboard.html?plugin=intellij&type=wtf&streamId=${settings.streamId}&readToken=${settings.readToken}'>your QD dashboard</a>.
+                       | <li>WTFs - Wtf is a way of <a href='http://www.quantifieddev.org/images/wtfs_per_minute.gif?plugin=intellij'>measuring code quality</a>. Hit the wtf button every time you see something you don't like or find confusing, then review you wtfs over time on <a href='http://www.quantifieddev.org/app/dashboard.html?plugin=intellij&type=wtf&streamId=$encodedStreamId&readToken=$encodedReadToken'>your QD dashboard</a>.
                        | </li>
-                       | <li>Builds - The plug-in logs when you start and complete builds and whether the build succeeded or failed. You can see your build behaviour over time on <a href='http://www.quantifieddev.org/app/dashboard.html?plugin=intellij&type=build&streamId=${settings.streamId}&readToken=${settings.readToken}'>your QD dashboard</a>.
+                       | <li>Builds - The plug-in logs when you start and complete builds and whether the build succeeded or failed. You can see your build behaviour over time on <a href='http://www.quantifieddev.org/app/dashboard.html?plugin=intellij&type=build&streamId=$encodedStreamId&readToken=$encodedReadToken'>your QD dashboard</a>.
                        | </li>
                        | <li>Community - The plug-in senses your location from your IP address and attaches this information to your logged events. This means that you can <a href='http://www.quantifieddev.org/app/community.html?plugin=intellij'>see yourself and your community of developers logging builds and WTFs around the world in real time</a>.
                        | </li>
@@ -209,7 +202,9 @@ class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory {
         qdButton.addActionListener(new ActionListener() {
             @Override
             void actionPerformed(ActionEvent e) {
-                def queryParams = "?streamId=${settings.streamId}&readToken=${settings.readToken}"
+                def encodedStreamId = URLEncoder.encode(settings.streamId, "UTF-8")
+                def encodedReadToken = URLEncoder.encode(settings.readToken, "UTF-8")
+                def queryParams = "?streamId=$encodedStreamId&readToken=$encodedReadToken"
                 Desktop.getDesktop().browse(new URL(Configuration.QD_DASHBOARD_URL + queryParams).toURI())
             }
         })
