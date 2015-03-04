a8209c37a602538f3eeeacc26515b379b71b6812
Devika Deshmukh <ddeshmukh@equalexperts.com>
2015-03-04 13:48:30 +0530

    -- Bumped up the version

--- src/main/groovy/AppConfig.groovy
+++ src/main/groovy/AppConfig.groovy
@@ -18,7 +18,7 @@ configuration {
         //JBoss Versioning Convention
         version {
             major = 0  //number related to production release
-            minor = 14 //changes or feature additions
+            minor = 15 //changes or feature additions
             micro = 1  //patches and bug fixes
             previous = 'None'
             complete = toCompleteVersion()
